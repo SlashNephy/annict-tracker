d713c3f232cd8691e37f32506b89c0a6a71fe626
SlashNephy <spica@starry.blue>
2022-10-08 14:03:12 +0900

    ignored-build-step.sh

---
+++ vercel-ignored-build-step.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]]; then
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
