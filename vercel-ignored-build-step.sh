cf3794a523a4432c4a430c6a9240c4f685027f41
SlashNephy <spica@starry.blue>
2022-10-08 14:21:37 +0900

    ブランチの指定間違ってた

--- vercel-ignored-build-step.sh
+++ vercel-ignored-build-step.sh
@@ -2,10 +2,12 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]]; then
-  echo "✅ - Build can proceed"
-  exit 1;
-else
-  echo "🛑 - Build cancelled"
-  exit 0;
-fi
+for branch in "master" "main" "develop" "dev" "staging"; do
+  if [[ "$VERCEL_GIT_COMMIT_REF" == "$branch" ]]; then
+    echo "✅ - Build can proceed"
+    exit 1;
+  else
+    echo "🛑 - Build cancelled"
+    exit 0;
+  fi
+done
