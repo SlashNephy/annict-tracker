f87d57e91e2ef7f0a49a6e605c526daa33e6eb38
SlashNephy <spica@starry.blue>
2022-11-02 16:15:22 +0900

    再放送のバッジを追加

--- components/work/DateBadge.tsx
+++ components/work/DateBadge.tsx
@@ -4,36 +4,52 @@ import React from 'react'
 import { useLibraryEntry } from '../../lib/useLibraryEntry'
 
 import type { BadgeProps } from '@mantine/core'
+import type { ReactNode } from 'react'
 
 export const DateBadge: React.FC<Omit<BadgeProps, 'color'>> = (props) => {
   const { entry } = useLibraryEntry()
 
+  const badges = [] as ReactNode[]
   switch (entry.timeTag) {
     case 'yesterday':
-      return (
+      badges.push(
         <Badge {...props} color="green">
           昨日
         </Badge>
       )
+      break
     case 'today':
-      return (
+      badges.push(
         <Badge {...props} color="red">
           今日
         </Badge>
       )
+      break
     case 'tomorrow':
-      return (
+      badges.push(
         <Badge {...props} color="blue">
           明日
         </Badge>
       )
+      break
     case 'finished':
-      return (
+      badges.push(
         <Badge {...props} color="green">
           終了
         </Badge>
       )
+      break
     default:
-      return null
+      break
   }
+
+  if (entry.nextProgram?.rebroadcast === true) {
+    badges.push(
+      <Badge {...props} color="yellow" variant="outline">
+        再放送
+      </Badge>
+    )
+  }
+
+  return <>{badges}</>
 }
