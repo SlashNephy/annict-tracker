698a98c47a539ab454601041aa067b0a25d726c6
SlashNephy <spica@starry.blue>
2023-08-25 21:58:42 +0900

    開発環境では recoil の警告を無視する

--- next.config.js
+++ next.config.js
@@ -8,4 +8,9 @@ if (process.env.DOCKER === '1') {
   nextConfig.output = 'standalone'
 }
 
+// 開発環境では recoil の警告を無視する
+if (process.env.NODE_ENV === 'development') {
+  process.env.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = 'false'
+}
+
 module.exports = nextConfig
