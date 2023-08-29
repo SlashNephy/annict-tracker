e0ee6bc5e5c01a58aa8eef90b8fcdcf122d2cbe2
SlashNephy <spica@starry.blue>
2023-08-29 17:23:53 +0900

    SSR 時には localStorage はない

--- lib/atoms.ts
+++ lib/atoms.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable @typescript-eslint/naming-convention */
-
 import { atom, selector } from 'recoil'
 import { recoilPersist } from 'recoil-persist'
 
@@ -9,66 +6,72 @@ import { SeasonName } from '../graphql/annict/generated/graphql.ts'
 import type { DayTag, TimeTag } from '../models/filters'
 import type { RecoilState } from 'recoil'
 
-const { persistAtom } = recoilPersist()
+function isServer(): boolean {
+  return typeof window !== typeof undefined
+}
+
+const { persistAtom } = recoilPersist({
+  storage: isServer() ? window.localStorage : undefined,
+})
 
 export const isOnlyCurrentSeasonState = atom<boolean>({
   key: 'only-current-season',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const hideRebroadcastingState = atom<boolean>({
   key: 'hide-rebroadcasting',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const hideStreamingServicesState = atom<boolean>({
   key: 'hide-video-services',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableSyobocalState = atom<boolean>({
   key: 'enable-syobocal',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const seasonFiltersState = atom<SeasonName[]>({
   key: 'season-filters',
   default: [SeasonName.Spring, SeasonName.Summer, SeasonName.Autumn, SeasonName.Winter],
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const timeFiltersState = atom<TimeTag[]>({
   key: 'time-filters',
   default: ['yesterday', 'today', 'tomorrow', 'finished'],
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const dayFiltersState = atom<DayTag[]>({
   key: 'day-filters',
   default: ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const syobocalChannelsState = atom<string[]>({
   key: 'syobocal-channels',
   default: [],
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableBrowserNotificationState = atom<boolean>({
   key: 'enable-browser-notification',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const programNotificationThresholdMinutesState = atom<number>({
   key: 'program-notification-threshold-minutes',
   default: 5,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const notificationHistoriesState = atom<string[]>({
@@ -79,73 +82,73 @@ export const notificationHistoriesState = atom<string[]>({
 export const isNavbarExpandState = atom<boolean>({
   key: 'is-navbar-expand',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableEverythingIntegrationState = atom<boolean>({
   key: 'enable-everything-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableEpgStationIntegrationState = atom<boolean>({
   key: 'enable-epgstation-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const epgStationUrlState = atom<string>({
   key: 'epgstation-url',
   default: 'http://localhost:8888',
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableDanimeIntegrationState = atom<boolean>({
   key: 'enable-danime-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableDanimeNiconicoIntegrationState = atom<boolean>({
   key: 'enable-danime-niconico-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableAbemaIntegrationState = atom<boolean>({
   key: 'enable-abema-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableNetflixIntegrationState = atom<boolean>({
   key: 'enable-netflix-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enablePrimeVideoIntegrationState = atom<boolean>({
   key: 'enable-prime-video-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableNiconicoChannelIntegrationState = atom<boolean>({
   key: 'enable-niconico_channel-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableBandaiChannelIntegrationState = atom<boolean>({
   key: 'enable-bandai-channel-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export const enableYouTubeIntegrationState = atom<boolean>({
   key: 'enable-youtube-integration',
   default: false,
-  effects_UNSTABLE: [persistAtom],
+  effects: [persistAtom],
 })
 
 export type SearchIntegrationKey =
