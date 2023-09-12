4e26db6771b6d872a6cf0206fc73237e930d16da
SlashNephy <spica@starry.blue>
2023-09-13 02:20:30 +0900

    /api/syobocal を廃止して Worker を参照させる

--- lib/services/syobocal.ts
+++ lib/services/syobocal.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable @typescript-eslint/naming-convention */
 
-import type { SyobocalRequest } from '../../pages/api/syobocal.ts'
+import type { SyobocalProgramsResponse } from '../../functions/api/syobocal/programs.ts'
 
 export type SyobocalProgram = {
   LastUpdate: string
@@ -33,63 +33,16 @@ export type SyobocalProgramLookupResult = {
   }
 }
 
-export type SyobocalTitle = {
-  TID: number
-  LastUpdate: string
-  Title: string
-  ShortTitle: string
-  TitleYomi: string
-  TitleEN: string
-  Comment: string
-  Cat: number
-  TitleFlag: number
-  FirstYear: number
-  FirstMonth: number
-  FirstEndYear: number
-  FirstEndMonth: number
-  FirstCh: string
-  Keywords: string
-  UserPoint: number
-  UserPointRank: number
-  SubTitles: string
-}
-
-type SyobocalTitleLookupResult = {
-  TitleLookupResponse?: {
-    TitleItems?: {
-      TitleItem?: SyobocalTitle[]
-    }
-    Result: {
-      Code: number
-      Message: string
-    }
-  }
-}
-
 export const lookupPrograms = async (tids: number[]): Promise<SyobocalProgramLookupResult> => {
-  const response = await fetch('/api/syobocal', {
-    method: 'POST',
+  const response = await fetch(`https://annict-tracker.pages.dev/api/syobocal/programs?id=${tids.join(',')}`, {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      Command: 'ProgLookup',
-      TID: tids.join(','),
-    } as SyobocalRequest),
   })
-  return response.json()
-}
+  const data = (await response.json()) as SyobocalProgramsResponse
+  if (!data.success) {
+    throw new Error(data.error)
+  }
 
-export const lookupTitles = async (tids: number[]): Promise<SyobocalTitleLookupResult> => {
-  const response = await fetch('/api/syobocal', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      Command: 'TitleLookup',
-      TID: tids.join(','),
-    } as SyobocalRequest),
-  })
-  return response.json()
+  return data.result
 }
