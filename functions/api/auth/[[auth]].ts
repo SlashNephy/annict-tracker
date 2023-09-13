7f669436537da292baa0b9fbd7ed07d91660c48d
SlashNephy <spica@starry.blue>
2023-09-14 01:13:19 +0900

    @auth/core による OAuth を実装する

---
+++ functions/api/auth/[[auth]].ts
@@ -0,0 +1,67 @@
+import { Auth } from '@auth/core'
+
+import type { OAuthConfig } from '@auth/core/providers'
+
+type Env = {
+  AUTH_SECRET: string
+  ANNICT_CLIENT_ID: string
+  ANNICT_CLIENT_SECRET: string
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const request = new Request(context.request.url)
+
+  return await Auth(request, {
+    providers: [
+      {
+        id: 'annict',
+        name: 'Annict',
+        type: 'oauth',
+        clientId: context.env.ANNICT_CLIENT_ID,
+        clientSecret: context.env.ANNICT_CLIENT_SECRET,
+        authorization: {
+          url: 'https://api.annict.com/oauth/authorize',
+          params: {
+            response_type: 'code',
+            scope: 'read write',
+          },
+        },
+        token: 'https://api.annict.com/oauth/token',
+        userinfo: 'https://api.annict.com/v1/me',
+        profile(profile) {
+          return {
+            id: profile.id.toString(),
+            email: profile.email,
+            name: profile.name,
+            image: profile.avatar_url,
+          }
+        },
+      } satisfies OAuthConfig<AnnictProfile>,
+    ],
+    secret: context.env.AUTH_SECRET,
+    session: {
+      strategy: 'jwt',
+    },
+  })
+}
+
+type AnnictProfile = {
+  id: number
+  username: string
+  name: string
+  description: string
+  url: string
+  avatar_url: string
+  background_image_url: string
+  records_count: number
+  followings_count: number
+  followers_count: number
+  wanna_watch_count: number
+  watching_count: number
+  watched_count: number
+  on_hold_count: number
+  stop_watching_count: number
+  created_at: string
+  email: string
+  notifications_count: number
+}
