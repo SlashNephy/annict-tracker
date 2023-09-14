302e3c3d30b89897dd87f10f3209f385089067fc
SlashNephy <spica@starry.blue>
2023-09-15 02:41:41 +0900

    関数スタイルにする

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-import type { UserConfig } from 'vite'
-
 // https://vitejs.dev/config/
-export default (): UserConfig => {
+export default defineConfig(() => {
   // 環境変数を詰め替える
   process.env.VITE_CF_PAGES_COMMIT_SHA = process.env.CF_PAGES_COMMIT_SHA
 
-  return defineConfig({
+  return {
     plugins: [react()],
-  })
-}
+  }
+})
