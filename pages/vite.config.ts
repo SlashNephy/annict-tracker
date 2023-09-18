4f1766094c83d86d90abe27dec3653d0ee851b68
SlashNephy <spica@starry.blue>
2023-09-18 19:11:37 +0900

    ソースマップをアップロードできるようにする

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -1,5 +1,6 @@
 import * as process from 'process'
 
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import relay from 'vite-plugin-relay'
@@ -11,6 +12,17 @@ export default defineConfig(() => {
   process.env.VITE_CF_PAGES_BRANCH = process.env.CF_PAGES_BRANCH
 
   return {
-    plugins: [react(), relay],
+    plugins: [
+      react(),
+      relay,
+      sentryVitePlugin({
+        org: 'starrybluesky',
+        project: 'annict-tracker',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    build: {
+      sourcemap: true,
+    },
   }
 })
