2942b0098476afc7e2a9ab0e6181ebcad3b25463
SlashNephy <spica@starry.blue>
2023-09-18 18:55:41 +0900

    Sentry 導入

--- pages/src/main.tsx
+++ pages/src/main.tsx
@@ -1,9 +1,18 @@
+import { init, BrowserTracing } from '@sentry/react'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
 import { App } from './App.tsx'
 import { router } from './router.tsx'
+
+if (import.meta.env.PROD) {
+  init({
+    dsn: 'https://406bfa633652c954c74eb4251033d857@o4505899258871808.ingest.sentry.io/4505900803686400',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 0.2,
+  })
+}
 
 const root = document.getElementById('root')
 if (root !== null) {
