decd1ddcf2f70857ad820f1d00e61a2cd3a92df1
SlashNephy <spica@starry.blue>
2023-09-24 23:21:44 +0900

    useSWR のキャッシュキーを修正

--- pages/src/lib/annict/useWorkImage.ts
+++ pages/src/lib/annict/useWorkImage.ts
@@ -32,39 +32,40 @@ export function useWorkImage(entryRef: useWorkImage_LibraryEntry$key): WorkImage
     entryRef
   )
 
+  // useSWR のキャッシュキーにクラスインスタンスを渡すと上手く動かないので外で事前計算しておく
   const arm = useArmSupplementaryDatastore()
-  const armEntry = useMemo(() => arm?.findByAnnictId(work.annictId), [work.annictId, arm])
+  const [aniListId, malId] = useMemo(() => {
+    const armEntry = arm?.findByAnnictId(work.annictId)
 
-  const aniListId = useMemo(() => armEntry?.anilist_id, [armEntry])
-  const malId = useMemo(() => armEntry?.mal_id?.toString() ?? work.malAnimeId, [armEntry, work.malAnimeId])
+    return [armEntry?.anilist_id, armEntry?.mal_id?.toString() ?? work.malAnimeId]
+  }, [arm, work])
 
-  const { data: imageUrl } = useSWRImmutable([`work-image-${work.annictId}`, aniListId, malId], async () => {
-    const initialImageUrl = work.image?.recommendedImageUrl
-
-    // Mixed Contents にならない場合はそのまま返す
-    if (initialImageUrl?.startsWith('https://')) {
-      return initialImageUrl
-    }
-
-    // AniList から画像を引いてみる
-    if (aniListId) {
-      const response = await fetchAniListImage(aniListId)
-      if (response.data.Media.coverImage.extraLarge) {
-        return response.data.Media.coverImage.extraLarge
+  const { data } = useSWRImmutable(
+    [work.image?.recommendedImageUrl, aniListId, malId],
+    async ([initialImageUrl, aniListId, malId]) => {
+      // Mixed Contents にならない場合はそのまま返す
+      if (initialImageUrl?.startsWith('https://')) {
+        return initialImageUrl
       }
 
-      console.log(response)
-    }
+      // AniList から画像を引いてみる
+      if (aniListId) {
+        const response = await fetchAniListImage(aniListId)
+        if (response.data.Media.coverImage.extraLarge) {
+          return response.data.Media.coverImage.extraLarge
+        }
+      }
 
-    // MyAnimeList から画像を引いてみる
-    if (malId) {
-      const response = await fetchJikanAnimePictures(malId)
-      return response.data[0]?.webp?.large_image_url ?? response.data[0]?.jpg?.large_image_url
+      // MyAnimeList から画像を引いてみる
+      if (malId) {
+        const response = await fetchJikanAnimePictures(malId)
+        return response.data[0]?.webp?.large_image_url ?? response.data[0]?.jpg?.large_image_url
+      }
     }
-  })
+  )
 
   return {
-    url: imageUrl,
+    url: data,
     copyright: work.image?.copyright ?? undefined,
   }
 }
--- pages/src/lib/syobocal/useSyobocalPrograms.ts
+++ pages/src/lib/syobocal/useSyobocalPrograms.ts
@@ -1,4 +1,5 @@
 import { minutesToMilliseconds } from 'date-fns'
+import { useMemo } from 'react'
 import { graphql, useFragment } from 'react-relay'
 import useSWRImmutable from 'swr/immutable'
 
@@ -26,28 +27,24 @@ export function useSyobocalPrograms(entryRef: useSyobocalPrograms_LibraryEntry$k
     entryRef
   )
 
+  // useSWR のキャッシュキーにクラスインスタンスを渡すと上手く動かないので外で事前計算しておく
   const arm = useArmSupplementaryDatastore(enabled)
+  const syobocalTid = useMemo(() => arm?.findByAnnictId(work.annictId)?.syobocal_tid ?? work.syobocalTid, [arm, work])
+
   const { data } = useSWRImmutable(
-    `syobocal-program-${work.id}`,
-    async () => {
-      if (!enabled) {
-        return []
-      }
-
-      // 次のエピソードがない
-      if (!nextEpisode) {
-        return []
-      }
-
-      // しょぼいカレンダーとの紐付けがされていない
-      const syobocalTid = arm?.findByAnnictId(work.annictId)?.syobocal_tid ?? work.syobocalTid
-      if (!syobocalTid) {
+    [enabled, nextEpisode?.number, syobocalTid],
+    async ([enabled, nextEpisodeNumber, syobocalTid]) => {
+      // 以下の場合はしょぼいカレンダーの放送予定を参照しない
+      // - このフックで無効化されている
+      // - この作品の次のエピソードがない
+      // - この作品がしょぼいカレンダーとの紐付けがされていない
+      if (!enabled || !nextEpisodeNumber || !syobocalTid) {
         return []
       }
 
       // TODO: 1リクエストにまとめたい
       const response = await lookupSyobocalPrograms([syobocalTid])
-      return response.reverse().filter((x) => x.count === nextEpisode.number)
+      return response.reverse().filter((x) => x.count === nextEpisodeNumber)
     },
     {
       refreshInterval: minutesToMilliseconds(30),
