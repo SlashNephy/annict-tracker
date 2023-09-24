7727c957d018cff1cf7b25c1122ea7bcbce06b34
SlashNephy <spica@starry.blue>
2023-09-24 20:01:55 +0900

    Vercel 用の移行ページを追加 (Close #649)

---
+++ vercel/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
+import React from 'react'
+
+import type { Metadata } from 'next'
+
+// eslint-disable-next-line import/order
+import '@mantine/core/styles.css'
+
+export const metadata: Metadata = {
+  title: 'annict-tracker',
+  description: 'annict-tracker は、Annict での視聴記録を便利にする Web アプリケーションです。',
+}
+
+export type RootLayoutProps = {
+  children: React.JSX.Element
+}
+
+export default function RootLayout({ children }: RootLayoutProps): React.JSX.Element {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript defaultColorScheme="auto" />
+      </head>
+      <body>
+        <MantineProvider defaultColorScheme="auto">{children}</MantineProvider>
+      </body>
+    </html>
+  )
+}
---
+++ vercel/src/app/page.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { Anchor, Card, CardSection, Container, Space, Text, Title } from '@mantine/core'
+import { IconBrandGithub } from '@tabler/icons-react'
+import React, { useEffect } from 'react'
+
+export default function Home(): React.JSX.Element {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') {
+      return
+    }
+
+    const timeout = window.setTimeout(() => {
+      window.location.replace('https://annict-tracker.pages.dev')
+    }, 10000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  return (
+    <main>
+      <Container>
+        <Card
+          withBorder
+          p="xl"
+          radius="xs"
+          shadow="sm"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            textAlign: 'center',
+          }}
+        >
+          <CardSection m="md" pb="md">
+            <Title ta="center">annict-tracker</Title>
+          </CardSection>
+
+          <Text size="md">
+            annict-tracker は Annict での視聴記録を便利にする Web アプリケーションです。
+            <br />
+            annict-tracker は Vercel から Cloudflare Pages に移転しました。
+          </Text>
+
+          <Space h="lg" />
+
+          <Text>
+            10秒後に <Anchor href="https://annict-tracker.pages.dev">annict-tracker.pages.dev</Anchor> に遷移します。
+          </Text>
+
+          <Space h="lg" />
+
+          <Text>
+            ソースコードは{' '}
+            <Anchor href="https://github.com/SlashNephy/annict-tracker" target="_blank">
+              <IconBrandGithub size={16} style={{ display: 'inline-block', verticalAlign: 'middle' }} /> GitHub
+            </Anchor>{' '}
+            で公開しています。
+          </Text>
+        </Card>
+      </Container>
+    </main>
+  )
+}
