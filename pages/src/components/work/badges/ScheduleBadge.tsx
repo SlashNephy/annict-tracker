72ebb369de68c5948f02062520559b8d708b68ea
SlashNephy <spica@starry.blue>
2023-09-24 20:18:06 +0900

    バッジの色を調整する

--- pages/src/components/work/badges/ScheduleBadge.tsx
+++ pages/src/components/work/badges/ScheduleBadge.tsx
@@ -18,20 +18,20 @@ export function ScheduleBadge({ entryRef }: WorkScheduleBadgeProps): React.JSX.E
   switch (tag) {
     case 'yesterday':
       return (
-        <Badge key="yesterday" color="green.4">
+        <Badge key="yesterday" c="black" color="teal.3">
           昨日
         </Badge>
       )
     case 'today':
       return (
         <>
-          <Badge key="today" color="red.5">
+          <Badge key="today" color="red.6">
             今日
           </Badge>
           {/* 今日放送であっても終了していたら出す。番組の長さを30分に仮定している */}
           {nextProgram &&
             isAfter(new Date(), nextProgram.endAt ? nextProgram.endAt : add(nextProgram.startAt, { minutes: 30 })) && (
-              <Badge key="finished" color="green.5">
+              <Badge key="finished" c="black" color="teal.3">
                 終了
               </Badge>
             )}
@@ -39,13 +39,13 @@ export function ScheduleBadge({ entryRef }: WorkScheduleBadgeProps): React.JSX.E
       )
     case 'tomorrow':
       return (
-        <Badge key="tomorrow" color="blue">
+        <Badge key="tomorrow" color="blue.7">
           明日
         </Badge>
       )
     case 'finished':
       return (
-        <Badge key="finished" color="green.5">
+        <Badge key="finished" c="black" color="teal.3">
           終了
         </Badge>
       )
