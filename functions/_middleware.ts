6b832782af491a0d20e0360e637523e38f1af919
SlashNephy <spica@starry.blue>
2023-09-24 13:01:17 +0900

    https: ではないときにもリダイレクト

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,12 +1,14 @@
 import type { Env } from './env.ts'
 
-const productionHost = 'annict-tracker.pages.dev:443'
+const productionProtocol = 'https:'
+const productionHost = 'annict-tracker.pages.dev'
 
 export const onRequest: PagesFunction<Env> = async (context) => {
   // 本番環境で指定された OAuth 2 クライアントは使用できないため、本番環境にリダイレクトする
   if (context.env.NODE_ENV !== 'development') {
     const url = new URL(context.request.url)
-    if (url.host !== productionHost) {
+    if (url.protocol !== productionProtocol || url.host !== productionHost) {
+      url.protocol = productionProtocol
       url.host = productionHost
       return Response.redirect(url, 301)
     }
