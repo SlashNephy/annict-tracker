874a347f57314ba7ea758e1fed85d6cb736df4d5
SlashNephy <spica@starry.blue>
2023-10-04 02:30:39 +0900

    Navbar にフィードバックの導線を追加

--- pages/src/components/layout/AppLayout.tsx
+++ pages/src/components/layout/AppLayout.tsx
@@ -1,11 +1,12 @@
-import { Anchor, AppShell, Burger, Card, Container, Group, Text, ThemeIcon, Tooltip } from '@mantine/core'
+import { Anchor, AppShell, Burger, Card, Container, Group, Stack, Text, ThemeIcon, Tooltip } from '@mantine/core'
 import { useDisclosure, useDocumentTitle, useMediaQuery } from '@mantine/hooks'
-import { IconBrandGithub } from '@tabler/icons-react'
+import { IconBrandGithub, IconSpeakerphone } from '@tabler/icons-react'
 import React, { useMemo } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 import { useCheckUpdate } from '../../lib/workers/useCheckUpdate.ts'
 import { routes } from '../../routes/router.tsx'
+import { FeedbackModal } from '../error/FeedbackModal.tsx'
 
 export function AppLayout({ children }: React.PropsWithChildren): React.JSX.Element {
   const [isNavbarOpened, { toggle: toggleNavbar, close: closeNavbar }] = useDisclosure(false)
@@ -15,6 +16,8 @@ export function AppLayout({ children }: React.PropsWithChildren): React.JSX.Elem
   const route = useMemo(() => {
     return routes.find(({ route }) => route.path === location.pathname)
   }, [location.pathname])
+
+  const [isFeedbackModalOpened, { open: openFeedbackModal, close: closeFeedbackModal }] = useDisclosure(false)
 
   useDocumentTitle(route ? `${route.label} | annict-tracker` : 'annict-tracker')
 
@@ -37,23 +40,61 @@ export function AppLayout({ children }: React.PropsWithChildren): React.JSX.Elem
       </AppShell.Header>
 
       <AppShell.Navbar p="md">
-        {routes.map(({ route, label, icon: Icon }) => (
-          <Anchor key={route.path} component={Link} mb="lg" to={route.path} onClick={closeNavbar}>
+        <Stack gap="sm">
+          {routes.map(({ route, label, icon: Icon }) => (
+            <Anchor key={route.path} component={Link} mb="lg" to={route.path} onClick={closeNavbar}>
+              <Group>
+                <Tooltip label={label} position="right">
+                  <ThemeIcon variant="light">
+                    <Icon size={20} />
+                  </ThemeIcon>
+                </Tooltip>
+                {isMobile && <Text>{label}</Text>}
+              </Group>
+            </Anchor>
+          ))}
+        </Stack>
+        <Stack bottom={0} gap="sm" pos="absolute">
+          <Anchor
+            mb="lg"
+            onClick={() => {
+              openFeedbackModal()
+              closeNavbar()
+            }}
+          >
             <Group>
-              <Tooltip label={label} position="right">
+              <Tooltip label="フィードバック" position="right">
                 <ThemeIcon variant="light">
-                  <Icon size={20} />
+                  <IconSpeakerphone size={20} />
                 </ThemeIcon>
               </Tooltip>
-
-              {}
-              {isMobile && <Text>{label}</Text>}
+              {isMobile && <Text>フィードバック</Text>}
             </Group>
           </Anchor>
-        ))}
+
+          <Anchor
+            mb="lg"
+            onClick={() => {
+              window.open('https://github.com/SlashNephy/annict-tracker', '_blank')
+              closeNavbar()
+            }}
+          >
+            <Group>
+              <Tooltip label="GitHub" position="right">
+                <ThemeIcon variant="light">
+                  <IconBrandGithub size={20} />
+                </ThemeIcon>
+              </Tooltip>
+              {isMobile && <Text>GitHub</Text>}
+            </Group>
+          </Anchor>
+        </Stack>
       </AppShell.Navbar>
 
-      <AppShell.Main>{children}</AppShell.Main>
+      <AppShell.Main>
+        {children}
+        <FeedbackModal isOpened={isFeedbackModalOpened} onClose={closeFeedbackModal} />
+      </AppShell.Main>
 
       <AppShell.Footer pb="sm" pos="relative" withBorder={false}>
         <Container>
