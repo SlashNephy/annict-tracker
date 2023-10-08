a2b10513ce878fa91d2d2771cd05e85883afa4a0
SlashNephy <spica@starry.blue>
2023-10-09 00:54:46 +0900

    ServiceWorker を追加

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -4,6 +4,7 @@ import { sentryVitePlugin } from '@sentry/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
+import { VitePWA } from 'vite-plugin-pwa'
 import relay from 'vite-plugin-relay'
 
 // https://vitejs.dev/config/
@@ -27,6 +28,53 @@ export default defineConfig(() => {
         project: 'annict-tracker',
         authToken: process.env.SENTRY_AUTH_TOKEN,
         telemetry: false,
+        disable: process.env.NODE_ENV === 'development',
+      }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        strategies: 'generateSW',
+        manifest: {
+          id: 'annict-tracker',
+          name: 'annict-tracker',
+          short_name: 'annict-tracker',
+          description: 'Annict での視聴記録を便利にする Web アプリケーションです。',
+          theme_color: '#E64980',
+          // TODO: アイコンを作成する
+          icons: [],
+          lang: 'ja',
+          display: 'standalone',
+        },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: /.+\.(js|css|svg|map)$/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'asset-cache',
+                expiration: {
+                  maxAgeSeconds: 90 * 24 * 60 * 60,
+                },
+              },
+            },
+            {
+              urlPattern: /^https:\/\/(.+\.anilist\.co|cdn\.myanimelist\.net|image\.annict\.com)\/.+/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'work-image-cache',
+                expiration: {
+                  maxAgeSeconds: 180 * 24 * 60 * 60,
+                },
+              },
+            },
+          ],
+          navigateFallback: '/index.html',
+          navigateFallbackDenylist: [/^\/api/],
+          cleanupOutdatedCaches: true,
+        },
+        devOptions: {
+          enabled: true,
+        },
       }),
     ],
     build: {
