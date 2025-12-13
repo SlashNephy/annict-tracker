33649fbbf94ce8f326c979ac9a9316bec47d1284
SlashNephy <slashnephy@hatena.ne.jp>
2025-12-13 23:01:41 +0900

    ESLint 9 に移行

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { config } from '@slashnephy/eslint-config'
+import { globalIgnores } from 'eslint/config'
+
+export default config({}, [
+  globalIgnores([
+    'pages/src/__generated__/*.graphql.ts',
+    'pages/dist/',
+    'pages/vite.config.js',
+    'pages/vite.config.d.ts',
+    'pages/dev-dist/',
+
+    'vercel/out/',
+    'vercel/out/.next/',
+  ]),
+  { files: ['pages/src/**/*.{ts,tsx}'], rules: { 'import/no-cycle': 'off' } },
+  { files: ['vercel/**/*.{ts,tsx}'], rules: { 'react-refresh/only-export-components': 'off' } },
+  {
+    rules: {
+      '@stylistic/quote-props': 'off',
+      '@stylistic/brace-style': ['error', '1tbs'],
+    },
+  },
+])
